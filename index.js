c844f898103bfba8622e0f35d0ee937988c31d84
paviesh <spaviesh@rapidit-cloudbera.com>
2023-03-15 17:25:28 +0530

    final changes

--- index.js
+++ index.js
@@ -13,30 +13,29 @@ function excelSheetsCompare(sheet1, sheet2) {
   const data1 = XLSX.utils.sheet_to_json(sheet1, { header: 1 });
   const data2 = XLSX.utils.sheet_to_json(sheet2, { header: 1 });
 
-  const sheetdifferences = [];
-
+  const rowDifferences = {};
   const maxRow = Math.max(data1.length, data2.length);
   const maxCol = Math.max(data1[0].length, data2[0].length);
 
   const headers = data1[0]; // use the first row of sheet1 as headers
-
   for (let i = 1; i < maxRow; i++) { // start from the second row
+    const rowDiffs = []; // create an array for the differences in this row
     for (let j = 0; j < maxCol; j++) {
       const val1 = data1[i] && data1[i][j] !== undefined ? data1[i][j] : '';
       const val2 = data2[i] && data2[i][j] !== undefined ? data2[i][j] : '';
-      sheetdifferences.push({
-        row: i,
+      rowDiffs.push({
         col: headers[j],
         val1,
         val2
       });
     }
+    if (rowDiffs.length > 0) {
+      rowDifferences[i] = rowDiffs; // add the row's differences to the object
+    }
   }
 
-  return sheetdifferences;
+  return rowDifferences;
 }
-
-
 
 app.post('/compare-excel', upload.fields([{ name: 'file1' }, { name: 'file2' }]), (req, res) => {
   const file1 = req.files.file1[0];
@@ -48,23 +47,19 @@ app.post('/compare-excel', upload.fields([{ name: 'file1' }, { name: 'file2' }])
   const sheetNames1 = workbook1.SheetNames;
   const excelDataDifference = [];
 
-  for (const excelfileName of sheetNames1) {
-    const excelsheet1 = workbook1.Sheets[excelfileName];
-    const excelsheet2 = workbook2.Sheets[excelfileName];
-   
-    const excelDifference = excelSheetsCompare(excelsheet1, excelsheet2);
-    if (excelDifference && excelDifference.length > 0) {
+  for (const sheetName of sheetNames1) {
+    const sheet1 = workbook1.Sheets[sheetName];
+    const sheet2 = workbook2.Sheets[sheetName];
+
+    const rowDifferences = excelSheetsCompare(sheet1, sheet2);
+    if (Object.keys(rowDifferences).length > 0) {
       excelDataDifference.push({
-        excelDifference
+        rowDifferences
       });
     }
   }
 
-  const result = {
-    excelDataDifference
-  };
-
-  res.json(result);
+  res.json(excelDataDifference);
 });
 
 app.listen(3000, () => {
